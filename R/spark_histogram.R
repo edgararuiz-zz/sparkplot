d1316122e1dd0c0c6d5867082e95729d81e0caa1
edgararuiz <edgararuiz@gmail.com>
2017-03-20 18:25:27 -0500

    Original commit

---
+++ R/spark_histogram.R
@@ -0,0 +1,61 @@
+#' Computes the bins of the histogram in the server
+#'
+#' @export
+#' @param data Data frame or Spark table
+#' @param bins Number of bins for the Histogram. Defaults to 30.
+#' @param output Sets the type of output, defaults to 'data'. Possible values: 'data', 'ggplot'
+#'
+#' @details
+#'
+#' The function will use the first column in the data set.  Using the dplyr::select statement
+#' prior to calling spark_histogram will ease adding or removing a plotting step.  The 'data' value
+#' for the 'db' argument will return a data frame instead of a plot.
+#'
+#' @examples
+#' \dontrun{
+#' # This will return a 'ggplot' of a 30 bin histogram of the 'distance' field
+#' flights %>%
+#' filter(arr_delay < 100) %>%
+#'   select(distance) %>%
+#'   spark_histogram()
+#' }
+#'
+#'
+spark_histogram <- function(data, bins = 30, output = "ggplot") {
+
+    x_name <- colnames(data)[1]
+
+    data_prep <- dplyr::select_(data, x_field = x_name)
+    data_prep <- dplyr::filter(data_prep, !is.na(x_field))
+    data_prep <- dplyr::mutate(data_prep, x_field = as.double(x_field))
+
+    s <- dplyr::summarise(data_prep, max_x = max(x_field), min_x = min(x_field))
+    s <- dplyr::mutate(s, bin_value = (max_x - min_x)/bins)
+    s <- dplyr::collect(s)
+
+    new_bins <- as.numeric(c((0:(bins - 1) * s$bin_value) + s$min_x, s$max_x))
+
+    if (class(data)[1] == "tbl_spark") {
+        plot_table <- sparklyr::ft_bucketizer(data_prep, input.col = "x_field", output.col = "key_bin", splits = new_bins)
+        plot_table <- dplyr::group_by(plot_table, key_bin)
+        plot_table <- dplyr::tally(plot_table)
+        plot_table <- dplyr::collect(plot_table)
+    }
+
+
+    all_bins <- data.frame(key_bin = 0:(bins - 1), bin = 1:bins, bin_ceiling = head(new_bins, -1))
+
+    plot_table <- dplyr::full_join(plot_table, all_bins, by = "key_bin")
+    plot_table <- dplyr::arrange(plot_table, key_bin)
+    plot_table <- dplyr::mutate(plot_table, n = ifelse(!is.na(n), n, 0))
+    plot_table <- dplyr::select(plot_table, count = n, bin_values = bin_ceiling)
+
+    if (output == "ggplot") {
+      plot_table <- ggplot2::ggplot(data = plot_table) +
+        ggplot2::geom_bar(ggplot2::aes(bin_values, count), stat = "identity") +
+        ggplot2::labs(x = x_name)
+    }
+
+    return(plot_table)
+}
+
---
+++ R/spark_raster.R
@@ -0,0 +1,48 @@
+#' Computes the frequency of x, y intersections and concentrates them based on the resolution
+#'
+#' @export
+#' @param data Data frame or Spark table
+#' @param resolution Number of squares to return to plot as raster. Defaults to 300.
+#' @param output Sets the type of output, defaults to 'data'. Possible values: 'data', 'ggplot'
+#'
+#' @details
+#'
+#' The function will use the first two columns in the data set.  Using the dplyr::select statement
+#' prior to calling spark_raster will ease adding or removing a plotting step..  The 'data' value
+#' for the 'db' argument will return a data frame instead of a plot.
+#'
+#' @examples
+#' \dontrun{
+#' flights %>%
+#'   filter(arr_delay < 100) %>%
+#'   select(arr_delay, dep_delay) %>%
+#'   spark_raster()
+#' }
+#'
+spark_raster <- function(data, resolution = 300, output = "ggplot") {
+
+    x_field <- colnames(data)[1]
+    y_field <- colnames(data)[2]
+
+    data_prep <- dplyr::select_(data, x = x_field, y = y_field)
+    data_prep <- dplyr::filter(data_prep, !is.na(x), !is.na(y))
+
+    s <- dplyr::summarise(data_prep, max_x = max(x), max_y = max(y), min_x = min(x), min_y = min(y))
+    s <- dplyr::mutate(s, rng_x = max_x - min_x, rng_y = max_y - min_y, resolution = resolution)
+
+    s <- dplyr::collect(s)
+
+    counts <- dplyr::mutate(data_prep, res_x = round((x - s$min_x)/s$rng_x * resolution, 0), res_y = round((y -
+        s$min_y)/s$rng_y * resolution, 0))
+    counts <- dplyr::count(counts, res_x, res_y)
+    counts <- dplyr::collect(counts)
+
+    if (output == "ggplot") {
+        counts <- ggplot2::ggplot(data = counts) +
+          ggplot2::geom_raster(ggplot2::aes(res_x, res_y, fill = n)) +
+          ggplot2::labs(x = x_field, y = y_field) +
+          ggplot2::scale_fill_continuous(name = "Frequency")
+    }
+
+    return(counts)
+}
